cf247ec47f24870bc69014680b3685de17c9eba5
cquintero99 <ciroalfonsoqude@ufps.edu.co>
2023-07-26 11:41:38 -0500

    validar rol para cambiar de modulo

--- src/views/fetch/Seguridad/Seguridad.js
+++ src/views/fetch/Seguridad/Seguridad.js
@@ -14,7 +14,7 @@ const VerificarToken = () => {
     if (!tieneToken && location.pathname !== "/auth/login") {
       navigate("/auth/login");
     }
-  }, [tieneToken, location,navigate]);
+  }, [tieneToken, location, navigate]);
 
   // Si el usuario tiene un token o está en la página de inicio de sesión (login), muestra el contenido normalmente.
   // De lo contrario, muestra un mensaje o componente que indique que se está verificando el token.
@@ -27,41 +27,65 @@ const VerificarRol = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const tieneToken = localStorage.getItem("token") !== null;
- 
 
   useEffect(() => {
-    if(tieneToken){
+    if (tieneToken) {
       const usuario = JSON.parse(
         JSON.stringify(parseJwt(localStorage.getItem("token")))
       );
       const numberRol = usuario.roles.length;
-    const rol1 = usuario.roles[0].nombre.split("_")[1].toLowerCase();
+      const rol1 = usuario.roles[0].nombre.split("_")[1].toLowerCase();
 
-    if (!location.pathname.includes(rol1) && numberRol === 1) {
-      localStorage.clear()
-      navigate("/auth/login");
-    } else if (numberRol > 1) {
-      const rol2 = usuario.roles[0].nombre.split("_")[1].toLowerCase();
-      if (!location.pathname.includes(rol2)) {
-        localStorage.clear()
-        navigate("/auth/login");
+      if(numberRol>1){
+        const rol2 = usuario.roles[1].nombre.split("_")[1].toLowerCase();
+        console.log(location.pathname.includes(rol1))
+        console.log(location.pathname.includes(rol2))  
+        if(!location.pathname.includes(rol1) && !location.pathname.includes(rol2) ){
+          localStorage.clear();
+          navigate("/auth/login");
+        }else if(!location.pathname.includes(rol2) && !location.pathname.includes(rol1)){
+          localStorage.clear();
+          navigate("/auth/login");
+        }
+      }else{
+        console.log(location.pathname.includes(rol1))
+        if(!location.pathname.includes(rol1) && numberRol===1){
+          localStorage.clear();
+          navigate("/auth/login");
+        }
       }
+      /*
+      if (!location.pathname.includes(rol1) && numberRol === 1) {
+        localStorage.clear();
+        navigate("/auth/login");
+      } else if (numberRol > 1) {
+        const rol2 = usuario.roles[1].nombre.split("_")[1].toLowerCase();
+        if (!location.pathname.includes(rol2)) {
+          localStorage.clear();
+          navigate("/auth/login");
+        }
+      }
+      */
     }
-  }
   }, [location, navigate, tieneToken]); // Aquí se eliminan los corchetes y se agrega el arreglo de dependencias
 
   //navigate("/" + rol + "/index");
 };
 
-
 function parseJwt(token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
 
   return JSON.parse(jsonPayload);
 }
 
-export { VerificarToken ,VerificarRol};
+export { VerificarToken, VerificarRol };
